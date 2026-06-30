6c14a52cc28df53b4bc9a2de3879bb29d1d1d52e
paulomkenya0-hue <paulomkenya0@gmail.com>
2026-06-30 19:56:41 +0300

    Update biometric_helper.dart

--- lib/core/utils/biometric_helper.dart
+++ lib/core/utils/biometric_helper.dart
@@ -1,27 +1,4 @@
-import 'package:local_auth/local_auth.dart';
-
-/// BiometricHelper - Hutumia alama ya kidole/uso kuthibitisha mtumiaji (ikiwa kifaa kinaweza)
 class BiometricHelper {
-  static final _auth = LocalAuthentication();
-
-  static Future<bool> isAvailable() async {
-    try {
-      final canCheck = await _auth.canCheckBiometrics;
-      final isSupported = await _auth.isDeviceSupported();
-      return canCheck && isSupported;
-    } catch (_) {
-      return false;
-    }
-  }
-
-  static Future<bool> authenticate() async {
-    try {
-      return await _auth.authenticate(
-        localizedReason: 'Tumia alama ya kidole kuingia kwenye Ales Masaba Animal Feed',
-        options: const AuthenticationOptions(biometricOnly: true, stickyAuth: true),
-      );
-    } catch (_) {
-      return false;
-    }
-  }
+  static Future<bool> isAvailable() async => false;
+  static Future<bool> authenticate() async => false;
 }
