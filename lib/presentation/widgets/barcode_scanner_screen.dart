0c009b898294645386c1cbf6745fc9ee6d76f88f
paulomkenya0-hue <paulomkenya0@gmail.com>
2026-07-01 09:36:27 +0300

    Refactor BarcodeScannerScreen to StatelessWidget
    Refactored BarcodeScannerScreen to a StatelessWidget and removed camera functionality.

--- lib/presentation/widgets/barcode_scanner_screen.dart
+++ lib/presentation/widgets/barcode_scanner_screen.dart
@@ -1,72 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 import '../../core/theme/app_theme.dart';
 import '../../core/constants/strings_sw.dart';
 
-/// BarcodeScannerScreen - Hufungua kamera na kusoma Barcode/QR Code ya bidhaa
-class BarcodeScannerScreen extends StatefulWidget {
+/// BarcodeScannerScreen - Imezimwa kwa sasa (inahitaji ruhusa ya CAMERA)
+class BarcodeScannerScreen extends StatelessWidget {
   const BarcodeScannerScreen({super.key});
-
-  @override
-  State<BarcodeScannerScreen> createState() => _BarcodeScannerScreenState();
-}
-
-class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
-  final MobileScannerController _controller = MobileScannerController();
-  bool _handled = false;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _onDetect(BarcodeCapture capture) {
-    if (_handled) return;
-    if (capture.barcodes.isEmpty) return;
-    final value = capture.barcodes.first.rawValue;
-    if (value != null) {
-      _handled = true;
-      Navigator.pop(context, value);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(SW.barcode),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.flash_on),
-            onPressed: () => _controller.toggleTorch(),
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          MobileScanner(controller: _controller, onDetect: _onDetect),
-          Center(
-            child: Container(
-              width: 250,
-              height: 150,
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColors.primaryGreen, width: 3),
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 30,
-            left: 0,
-            right: 0,
-            child: const Text(
-              'Lenga barcode/QR ndani ya mraba',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-            ),
-          ),
-        ],
+      appBar: AppBar(title: const Text('Skana Barcode')),
+      body: const Center(
+        child: Text(
+          'Kipengele hiki kitapatikana hivi karibuni.',
+          style: TextStyle(color: AppColors.darkGreen, fontSize: 16),
+        ),
       ),
     );
   }
