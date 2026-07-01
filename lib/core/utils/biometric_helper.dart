bfe50316e66a4d7f3d9b7e2cbd9477bc25e43475
paulomkenya0-hue <paulomkenya0@gmail.com>
2026-07-01 08:45:23 +0300

    Add comment for BiometricHelper class
    Added a comment in Swahili explaining the purpose of the class.

--- lib/core/utils/biometric_helper.dart
+++ lib/core/utils/biometric_helper.dart
@@ -1,3 +1,4 @@
+// Biometric imezimwa - app inatumia nenosiri pekee
 class BiometricHelper {
   static Future<bool> isAvailable() async => false;
   static Future<bool> authenticate() async => false;
