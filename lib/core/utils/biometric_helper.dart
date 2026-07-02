d53dc7459e569d329b2205af80c8dc3e73dda67b
paulomkenya0-hue <paulomkenya0@gmail.com>
2026-07-02 14:43:27 +0300

    Update project with new version

--- lib/core/utils/biometric_helper.dart
+++
@@ -1,5 +0,0 @@
-// Biometric imezimwa - app inatumia nenosiri pekee
-class BiometricHelper {
-  static Future<bool> isAvailable() async => false;
-  static Future<bool> authenticate() async => false;
-}
